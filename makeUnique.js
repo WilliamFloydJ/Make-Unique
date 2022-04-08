8714939b122fc13ee077320103821a7aa693f704
WilliamFloydJ <floydfamily71@gmail.com>
2022-04-07 19:44:16 -0600

    Working Iteration

--- makeUnique.js
+++ makeUnique.js
@@ -1,1 +1,18 @@
 // Write your solution below:
+
+const makeUnique = (string) => {
+  let letArr = [];
+  const stringArr = string.split("");
+  stringArr.forEach((element) => {
+    if (
+      letArr.includes(element.toLowerCase()) === false &&
+      letArr.includes(element.toUpperCase()) === false
+    ) {
+      letArr.push(element);
+    }
+  });
+
+  return letArr.toString();
+};
+
+console.log(makeUnique("hhhavvveee aaNNIICCCCEEE DDDDAAAA YYY"));
